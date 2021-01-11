e9a6457e9fde69e22db357756af115146ea9ffcb
Gazyn <jurgen.sepp@khk.ee>
2021-01-11 09:48:33 +0200

    Ülesanne 1.10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// a proper place to define a component
+const Statistic = ({text, value}) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({good, neutral, bad, all}) => {
   if(all===0) {
     return (
@@ -14,15 +19,20 @@ const Statistics = ({good, neutral, bad, all}) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>average {(good-bad)/all}</p>
-      <p>total {good/all}</p>
+      <Statistic text="good" value = {good} />
+      <Statistic text="neutral" value = {neutral} />
+      <Statistic text="bad" value = {bad} />
+      <Statistic text="total" value = {(good-bad)/all} />
+      <Statistic text="average" value = {good/all} />
     </div>
   )
 }
 
+const Button = ({text, value, theFunction}) => {
+  return (
+    <button onClick={() => theFunction(value+1)}>{text}</button>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -33,9 +43,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=>setGood(good+1)}>good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <Button text={"good"} value={good} theFunction={setGood}/>
+      <Button text={"neutral"} value={neutral} theFunction={setNeutral}/>
+      <Button text={"bad"} value={bad} theFunction={setBad}/>
       <Statistics good={good} neutral={neutral} bad={bad} all={good+neutral+bad}/>
     </div>
   )
